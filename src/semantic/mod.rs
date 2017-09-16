4acb2bbe4f51ab304ec56fa3c1edde99f92052c8
Mario Nachbaur <marionauta@users.noreply.github.com>
2017-09-16 14:11:55 +0200

    Remove 'deps' and only use 'defs'

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -4,8 +4,7 @@ use std::collections::hash_set::HashSet;
 use super::parser::{Ast, Parser};
 
 pub struct SemanticBuilder {
-    defs: HashMap<String, Vec<Ast>>,
-    deps: HashMap<String, Vec<String>>,
+    defs: HashMap<String, Vec<(String, String)>>,
 
     order: Vec<String>,
     visited: HashSet<String>,
@@ -13,7 +12,7 @@ pub struct SemanticBuilder {
 }
 
 pub struct Semantic {
-    pub defs: HashMap<String, Vec<Ast>>,
+    pub defs: HashMap<String, Vec<(String, String)>>,
     pub order: Vec<String>,
     pub cycles: HashSet<String>,
 }
@@ -21,18 +20,15 @@ pub struct Semantic {
 impl SemanticBuilder {
     pub fn new(ast: Parser) -> Self {
         let mut defs = HashMap::new();
-        let mut deps = HashMap::new();
 
         for def in ast {
             if let Ast::TypeDefinition(name, parameters) = def {
-                deps.insert(name.clone(), build_deps(&parameters));
-                defs.insert(name, parameters);
+                defs.insert(name, build_defs(parameters));
             }
         }
 
         SemanticBuilder {
             defs,
-            deps,
             order: Vec::new(),
             visited: HashSet::new(),
             cycles: HashSet::new(),
@@ -40,7 +36,7 @@ impl SemanticBuilder {
     }
 
     pub fn build(mut self) -> Semantic {
-        for node in self.deps.clone().keys() {
+        for node in self.defs.clone().keys() {
             self.visit(node);
         }
 
@@ -62,11 +58,11 @@ impl SemanticBuilder {
 
         self.visited.insert(node.clone());
 
-        self.deps.clone().get(node).map(|d|{
-            for v in d {
+        if let Some(d) = self.defs.clone().get(node) {
+            for &(_, ref v) in d {
                 self.visit(v);
             }
-        });
+        }
 
         if !self.cycles.contains(node) {
             self.order.push(node.clone());
@@ -75,11 +71,15 @@ impl SemanticBuilder {
     }
 }
 
-fn build_deps(ast: &[Ast]) -> Vec<String> {
-    ast.iter()
-        .filter_map(|a| match *a {
-            Ast::Parameter(_, ref name) => Some(name.clone()),
-            _ => None,
-        })
+fn ast_to_tuple(ast: Ast) -> Option<(String, String)> {
+    match ast {
+        Ast::Parameter(name, typename) => Some((name, typename)),
+        _ => None,
+    }
+}
+
+fn build_defs(ast: Vec<Ast>) -> Vec<(String, String)> {
+    ast.into_iter()
+        .filter_map(ast_to_tuple)
         .collect()
 }
