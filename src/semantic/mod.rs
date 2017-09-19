016b517ff7e21c3f7e7a99e9b102c08cc63f013c
Mario Nachbaur <marionauta@users.noreply.github.com>
2017-09-19 12:03:44 +0200

    Add nicer API for semantic:
    - Now build 'Semantic' with 'Semantic::analyze()'.
    - 'SemanticBuilder' is private.
    - New 'SemanticError' enum type.
    - Building a semantic return a 'Result'

--- src/semantic/mod.rs
+++ src/semantic/mod.rs
@@ -1,56 +1,73 @@
 use std::collections::hash_map::HashMap;
 use std::collections::hash_set::HashSet;
 
+use super::lexer::Token;
 use super::parser::{Ast, Parser};
 
-pub struct SemanticBuilder {
-    defs: HashMap<String, Vec<(String, String)>>,
+pub enum SemanticError {
+    Unexpected(Vec<Token>),
+}
+
+pub struct Semantic {
+    pub definitions: HashMap<String, Vec<(String, String)>>,
+    pub order: Vec<String>,
+    pub cycles: HashSet<String>,
+}
+
+impl Semantic {
+    pub fn analyze(ast: Parser) -> Result<Semantic, SemanticError> {
+        let mut definitions = HashMap::new();
+        let mut errors = Vec::new();
+
+        for definition in ast {
+            match definition {
+                Ast::TypeDefinition(name, parameters) => {
+                    definitions.insert(name, build_parameters(parameters));
+                },
+                Ast::Unexpected(token) => errors.push(token),
+                _ => unreachable!(),
+            }
+        }
+
+        if !errors.is_empty() {
+            return Err(SemanticError::Unexpected(errors));
+        }
+
+        SemanticBuilder::build(definitions)
+    }
+}
+
+struct SemanticBuilder {
+    definitions: HashMap<String, Vec<(String, String)>>,
 
     order: Vec<String>,
     visited: HashSet<String>,
     cycles: HashSet<String>,
 }
 
-pub struct Semantic {
-    pub defs: HashMap<String, Vec<(String, String)>>,
-    pub order: Vec<String>,
-    pub cycles: HashSet<String>,
-}
-
 impl SemanticBuilder {
-    pub fn new(ast: Parser) -> Self {
-        let mut defs = HashMap::new();
-
-        for def in ast {
-            if let Ast::TypeDefinition(name, parameters) = def {
-                defs.insert(name, build_defs(parameters));
-            }
-        }
-
-        SemanticBuilder {
-            defs,
+    fn build(definitions: HashMap<String, Vec<(String, String)>>) -> Result<Semantic, SemanticError> {
+        let mut sb = SemanticBuilder {
+            definitions,
             order: Vec::new(),
             visited: HashSet::new(),
             cycles: HashSet::new(),
-        }
-    }
+        };
 
-    pub fn build(mut self) -> Semantic {
-        for node in self.defs.clone().keys() {
-            self.visit(node);
+        for node in sb.definitions.clone().keys() {
+            sb.visit(node);
         }
 
-        Semantic {
-            defs: self.defs,
-            order: self.order,
-            cycles: self.cycles,
-        }
+        Ok(Semantic {
+            definitions: sb.definitions,
+            order: sb.order,
+            cycles: sb.cycles,
+        })
     }
 
     fn visit(&mut self, node: &String) {
         if self.order.contains(node) {
             return;
-
         } else if self.visited.contains(node) {
             self.cycles = self.visited.clone();
             return;
@@ -58,7 +75,7 @@ impl SemanticBuilder {
 
         self.visited.insert(node.clone());
 
-        if let Some(d) = self.defs.clone().get(node) {
+        if let Some(d) = self.definitions.clone().get(node) {
             for &(_, ref v) in d {
                 self.visit(v);
             }
@@ -71,15 +88,15 @@ impl SemanticBuilder {
     }
 }
 
-fn ast_to_tuple(ast: Ast) -> Option<(String, String)> {
+fn ast_to_parameter(ast: Ast) -> Option<(String, String)> {
     match ast {
         Ast::Parameter(name, typename) => Some((name, typename)),
         _ => None,
     }
 }
 
-fn build_defs(ast: Vec<Ast>) -> Vec<(String, String)> {
+fn build_parameters(ast: Vec<Ast>) -> Vec<(String, String)> {
     ast.into_iter()
-        .filter_map(ast_to_tuple)
+        .filter_map(ast_to_parameter)
         .collect()
 }
